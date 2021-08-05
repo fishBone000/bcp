a44a57b8fe17b49d8809a7d9c1b6ddc6a7338b22
fishBone000 <FishBoneEK@outlook.com>
2021-08-05 13:56:04 +0000

    MultiBlock is now funtional. Note that it shouldn't be moved by any means. None of such tests has been run, and it's expected to be bugful!

--- java/group/sit/bcp/tileentity/MainBlockPosTE.java
+++ java/group/sit/bcp/tileentity/MainBlockPosTE.java
@@ -3,12 +3,17 @@ package group.sit.bcp.tileentity;
 
 import net.minecraft.util.math.BlockPos;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import group.sit.bcp.bcp;
 import net.minecraft.block.BlockState;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
 
 public class MainBlockPosTE extends TileEntity {
+
+	private static Logger LOGGER = LogManager.getLogger();
 
 	private BlockPos mainBlockPos;
 
@@ -30,14 +35,15 @@ public class MainBlockPosTE extends TileEntity {
 			nbt.putInt("mBPx", mainBlockPos.getX());
 			nbt.putInt("mBPy", mainBlockPos.getY());
 			nbt.putInt("mBPz", mainBlockPos.getZ());
+			LOGGER.debug("Writing CompoundNBT: " + nbt.toString());
 			return nbt;
 		}else
-			throw new RuntimeException(this.toString() + "'s mainBlockPos is null. This is a bug!");
+			throw new RuntimeException("MainBlockTE's mainBlockPos is null, which is at pos " + this.getPos().toString() + ". This is a bug!");
 	}
 
 	public void read(BlockState state, CompoundNBT nbt) {
 		super.read(state, nbt);
-		if(nbt.contains("mBPx") && nbt.contains("mBPy") && nbt.contains("nBPz")) {
+		if(nbt.contains("mBPx") && nbt.contains("mBPy") && nbt.contains("mBPz")) {
 			mainBlockPos = new BlockPos(nbt.getInt("mBPx"), nbt.getInt("mBPy"), nbt.getInt("mBPz"));
 		}else
 			throw new RuntimeException(nbt.toString() + "doesn't has desired mainBlockPos. This is a bug!");
