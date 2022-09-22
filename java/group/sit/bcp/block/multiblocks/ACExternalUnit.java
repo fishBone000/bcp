6d9f4a98274de4ab07f9103927d7f713296c4d8d
fishBone000 <FishBoneEK@outlook.com>
2022-09-22 14:52:16 +0000

    Resize and remodel for some multi blocks. Localization added for all multi blocks (except test_huge_block) Grey Bricks item model corrected.

--- java/group/sit/bcp/block/multiblocks/ACExternalUnit.java
+++ java/group/sit/bcp/block/multiblocks/ACExternalUnit.java
@@ -1,6 +1,7 @@
 package group.sit.bcp.block.multiblocks;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,7 +15,6 @@ public class ACExternalUnit extends MultiBlock {
 
 	protected static final double[][] AABB_PARAMETERS = {
 				{4D, 0D, 1D, 16D, 16D, 15D},
-				{0D, 0D, 1D, 16D, 16D, 15D},
 				{0D, 0D, 1D, 12D, 16D, 15D}
 		};
 	protected static final VoxelShape SHAPES[][] = HorizontalAABBGenerator.generate(AABB_PARAMETERS); 
@@ -23,14 +23,19 @@ public class ACExternalUnit extends MultiBlock {
 		super(properties);
 	}
 
-	protected int widthStart()	{ return -1; }
+	protected int widthStart()	{ return 0; }
 	protected int widthEnd()	{ return 1; }
 	protected int heightStart()	{ return 0; }
-	protected int heightEnd()	{ return 1; }
+	protected int heightEnd()	{ return 0; }
 	protected int depthStart()	{ return 0; }
 	protected int depthEnd()	{ return 0; }
 
 	public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
 		return SHAPES[HorizontalDirectionToIndex.map(pState.getValue(FACING))][pState.getValue(getIndexProperty())%3];
+	}
+
+	public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+		return super.getStateForPlacement(pContext)
+				.setValue(FACING, pContext.getHorizontalDirection().getOpposite());
 	}
 }
--- java/group/sit/bcp/block/multiblocks/AirConditioner.java
+++ java/group/sit/bcp/block/multiblocks/AirConditioner.java
@@ -4,6 +4,7 @@ import group.sit.bcp.utils.HorizontalAABBGenerator;
 import group.sit.bcp.utils.HorizontalDirectionToIndex;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Rotation;
@@ -18,7 +19,7 @@ public class AirConditioner extends MultiBlock{
 		super(properties);
 	}
 
-	protected int widthStart()	{ return -1; }
+	protected int widthStart()	{ return 0; }
 	protected int widthEnd()	{ return 1; }
 	protected int heightStart()	{ return 0; }
 	protected int heightEnd()	{ return 0; }
@@ -26,9 +27,8 @@ public class AirConditioner extends MultiBlock{
 	protected int depthEnd()	{ return 0; }
 
 	protected static final double AABB_PARAM[][] = {
-			{ 1D, 1D, 0D, 16D, 15D, 15D }, 
-			{ 0D, 1D, 0D, 16D, 15D, 15D }, 
-			{ 0D, 1D, 0D, 15D, 15D, 15D }
+			{ 1D, 1D, 1D, 16D, 15D, 16D }, 
+			{ 0D, 1D, 1D, 15D, 15D, 16D }
 	};
 
 	protected static final VoxelShape SHAPES[][] = HorizontalAABBGenerator.generate(AABB_PARAM);
@@ -42,9 +42,9 @@ public class AirConditioner extends MultiBlock{
 		Rotation rotation = facingToRotation(pState);
 		Direction facing = pState.getValue(FACING);
 		for(int iWidth = widthStart; iWidth <= widthEnd; iWidth++) {
-			BlockPos iPos = pPos.offset(new BlockPos(iWidth, 0, 0).rotate(rotation).relative(facing));
+			BlockPos iPos = pPos.offset(new BlockPos(iWidth, 0, 0).rotate(rotation).relative(facing.getOpposite()));
 			BlockState state = pLevel.getBlockState(iPos);
-			if(!state.isFaceSturdy(pLevel, iPos, facing.getOpposite()))
+			if(!state.isFaceSturdy(pLevel, iPos, facing))
 				return false;
 		}
 		return true;
@@ -52,6 +52,11 @@ public class AirConditioner extends MultiBlock{
 
 	public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
 		return SHAPES[HorizontalDirectionToIndex.map(pState.getValue(FACING))][pState.getValue(getIndexProperty())];
+	}
+
+	public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+		return super.getStateForPlacement(pContext)
+				.setValue(FACING, pContext.getHorizontalDirection().getOpposite());
 	}
 
 }
